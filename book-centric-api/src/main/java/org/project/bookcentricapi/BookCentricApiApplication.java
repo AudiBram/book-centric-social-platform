0e76bb213ddda0ea639b19b7b012d97d3c207639
Audi Bramastya <audibram14@gmail.com>
2024-04-16 08:36:37 +0700

    Add backend project

---
+++ book-centric-api/src/main/java/org/project/bookcentricapi/BookCentricApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.bookcentricapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCentricApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCentricApiApplication.class, args);
+    }
+
+}
---
+++ book-centric-api/src/test/java/org/project/bookcentricapi/BookCentricApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.bookcentricapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCentricApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
