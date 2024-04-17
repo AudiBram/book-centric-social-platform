ba3873db0491040e9699ee3917688ab571ed02bd
Audi Bramastya <audibram14@gmail.com>
2024-04-17 11:53:40 +0700

    Add user, token and role entity with repository

--- book-centric-api/src/main/java/org/project/bookcentricapi/BookCentricApiApplication.java
+++ book-centric-api/src/main/java/org/project/bookcentricapi/BookCentricApiApplication.java
@@ -2,8 +2,10 @@ package org.project.bookcentricapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookCentricApiApplication {
 
     public static void main(String[] args) {
