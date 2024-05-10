573ab9e57535b6fa76593726409673b33e785ca7
Audi Bramastya <audibram14@gmail.com>
2024-05-10 12:54:42 +0700

    Implements fileutils for book mapper

--- book-centric-api/src/main/java/org/project/bookcentricapi/book/BookMapper.java
+++ book-centric-api/src/main/java/org/project/bookcentricapi/book/BookMapper.java
@@ -1,5 +1,6 @@
 package org.project.bookcentricapi.book;
 
+import org.project.bookcentricapi.file.FileUtils;
 import org.project.bookcentricapi.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().fullname())
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
                 .build();
     }
 
